2dc5c088621ced565d27ec7e7ca9b4fdcb52c5af
EGORIKBOCHA2006 <be4364938@yandex.ru>
2022-04-17 22:21:15 +0300

    Update index.py

--- index.py
+++ index.py
@@ -1,5 +1,6 @@
-a=1
-print(a)
-while a!=10:
-    a+=1
-    print(a)
+a=int(input("stolbcov"))
+b=int(input("strok"))
+for i in range(1,b+1):
+    for i in range(1,a+1):
+        print("*",end="")
+    c=input("")
